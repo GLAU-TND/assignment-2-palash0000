5e2abd5dbe5a46e23b6cfeeaad6ebd9c716832e6
palash0000 <palash.goyal_cs18gla.ac.in>
2020-03-28 18:40:58 +0530

    final commit

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,43 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+    private Node front;
+    private Node rear;
+
+    public MyPriorityQueue() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+    public void insert(int data, int priority) {
+        Node n = new Node(data, priority);
+        if (isEmpty() || n.getPriority() < front.getPriority()) {
+            n.setNext(front);
+            front = n;
+        } else {
+            Node tem = front;
+            while (tem.getNext() != null && tem.getNext().getPriority() <= n.getPriority()) {
+                tem = tem.getNext();
+            }
+            n.setNext(tem.getNext());
+            tem.setNext(n);
+        }
+    }
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("underflow");
+        } else {
+            Node tem = front;
+            while (tem != null) {
+                System.out.println(tem.getData() + " " + tem.getPriority());
+                tem = tem.getNext();
+            }
+        }
+    }
 }
